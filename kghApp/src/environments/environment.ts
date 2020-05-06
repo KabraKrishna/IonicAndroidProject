4cc854c77298da129b9e222b23e0607dc59bd0fa
kkabra <kkabra@users.github.com>
2020-05-06 09:47:54 +0530

    First Commit, basic layout of the application

---
+++ kghApp/src/environments/environment.ts
@@ -0,0 +1,25 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebaseConfig : {
+    apiKey: "AIzaSyAImeF8b3gjf2fTp3Kh2edEpvNuaZb9nIY",
+    authDomain: "kgh-application.firebaseapp.com",
+    databaseURL: "https://kgh-application.firebaseio.com",
+    projectId: "kgh-application",
+    storageBucket: "kgh-application.appspot.com",
+    messagingSenderId: "265209207241",
+    appId: "1:265209207241:web:12e772e231012d38dbf018"
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
